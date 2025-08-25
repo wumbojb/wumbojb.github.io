a50ed92d91fc05d83944bde6eca1ccf968e9a80d
Masputrawae <masputrawae.official@gmail.com>
2025-08-25 16:38:40 +0700

    Changed On 📅 : 2025-08-25 16:38:40

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,12 +10,17 @@ const sidebarHandler = () => {
 
 const tabHandler = () => {
   const btns = document.querySelectorAll("[data-tab]");
+  const btnsClass = document.querySelectorAll(".tab-control__btn");
 
   btns.forEach((btn) => {
     const targetId = btn.getAttribute("data-tab");
     const tabPane = document.getElementById(targetId);
+    const activeBtn = document.querySelector(`.tab-control__btn[data-tab="${targetId}"]`);
 
     btn.addEventListener("click", () => {
+      btnsClass.forEach((e) => e.classList.remove("tab-control__btn--is-active"));
+      activeBtn.classList.add("tab-control__btn--is-active");
+      
       document
         .querySelectorAll(".tab-pane")
         .forEach((e) => e.classList.remove("tab-pane--is-active"));
